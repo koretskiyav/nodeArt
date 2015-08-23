f5a575525ad8681fede40015b0fbc0a3cbfea780
gudvin <koretskiyav@gmail.com>
2015-08-23 11:14:17 +0300

    test OK

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express         = require('express');
+var path            = require('path');
 
-var app = express();
+var app = express.createServer();
+
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/test', function (req, res) {
+    res.send('test OK');
+});
 
 app.listen(8080, function(){
     console.log('Express server listening on port 8080');
