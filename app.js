425a5aecf8db748e39e3fefd600ef6f76d9d4055
gudvin <koretskiyav@gmail.com>
2015-08-23 12:30:56 +0300

    add config, mongoose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express         = require('express');
 var path            = require('path');
 var log             = require('./libs/log')(module);
+var config          = require('./libs/config');
+var Contact         = require('./libs/mongoose').Contact;
 
 var app = express.createServer();
 
@@ -14,6 +16,20 @@ app.get('/test', function (req, res) {
     res.send('test OK');
 });
 
-app.listen(8080, function(){
-    log.info('Express server listening on port 8080');
+app.use(function(req, res, next){
+    res.status(404);
+    log.debug('Not found URL: %s',req.url);
+    res.send({ error: 'Not found' });
+    return;
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    log.error('Internal error(%d): %s',res.statusCode,err.message);
+    res.send({ error: err.message });
+    return;
+});
+
+app.listen(config.get('port'), function(){
+    log.info('Express server listening on port ' + config.get('port'));
 });
