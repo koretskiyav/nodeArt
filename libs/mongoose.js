edc6ea0cb0975175dbc7d32f028625c67b960fbd
gudvin <koretskiyav@gmail.com>
2015-08-24 11:19:10 +0300

    add UserShema

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -30,4 +30,47 @@ var ContactSchema = new Schema({
 
 var Contact = mongoose.model('Contact', ContactSchema);
 
+var crypto      = require('crypto');
+
+// User
+var User = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String,
+        required: true
+    }
+});
+
+User.methods.encryptPassword = function(password) {
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+};
+
+User.virtual('userId')
+    .get(function () {
+        return this.id;
+    });
+
+User.virtual('password')
+    .set(function(password) {
+        this._plainPassword = password;
+        this.salt = crypto.randomBytes(32).toString('base64');
+        this.hashedPassword = this.encryptPassword(password);
+    })
+    .get(function() { return this._plainPassword; });
+
+
+User.methods.checkPassword = function(password) {
+    return this.encryptPassword(password) === this.hashedPassword;
+};
+
+var UserModel = mongoose.model('User', User);
+
 module.exports.Contact = Contact;
